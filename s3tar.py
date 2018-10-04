1eeb3bae8b876ead1294875250dcee553717ecc3
Sid Stuart <sid.stuart@sentient.ai>
2018-10-04 13:16:34 -0700

    More issues from the cut and paste.

--- s3tar.py
+++ s3tar.py
@@ -185,7 +185,8 @@ def extract_bucket(bucket_name, new_bucket_name, archive_name, s3, s3_client, pr
 		for object in archive_bucket.objects.filter(Prefix=bucket_name):
 			not_found = False
 			print(object.key)
-			archive_in = smart_open.smart_open(object.key, 'rb', profile_name=profile)
+			tar_name = 's3://%s/%s_file%d%s' % (archive_name, object.key)
+			archive_in = smart_open.smart_open(tar_name, 'rb', profile_name=profile)
 			# Need to test for zip file and set mode.
 			mode = get_compressed_mode(object.key)
 			tf = tarfile.open(mode=mode, fileobj=archive_in)
